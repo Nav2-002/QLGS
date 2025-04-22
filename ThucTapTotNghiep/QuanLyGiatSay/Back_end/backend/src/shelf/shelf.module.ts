ba39435f7c5bae35a905951d7f7296e1c8d296d6
NMTThinh <129759845+NMTThinh@users.noreply.github.com>
2025-04-22 16:54:16 +0700

    Update shelf.module.ts

--- ThucTapTotNghiep/QuanLyGiatSay/Back_end/backend/src/shelf/shelf.module.ts
+++ ThucTapTotNghiep/QuanLyGiatSay/Back_end/backend/src/shelf/shelf.module.ts
@@ -3,12 +3,21 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ShelfController } from './shelf.controller';
 import { ShelfService } from './shelf.service';
 import { Shelf, ShelfSchema } from './model/shelf.schema';
+import { ShelfRepository } from './shelf.repository';
 
+// @Module({
+//   imports: [
+//     MongooseModule.forFeature([{ name: Shelf.name, schema: ShelfSchema }]),
+//   ],
+//   controllers: [ShelfController],
+//   providers: [ShelfService],
+// })
+// export class ShelfModule {}
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Shelf.name, schema: ShelfSchema }]),
   ],
   controllers: [ShelfController],
-  providers: [ShelfService],
+  providers: [ShelfService, ShelfRepository], // Thêm ShelfRepository vào đây
 })
 export class ShelfModule {}
