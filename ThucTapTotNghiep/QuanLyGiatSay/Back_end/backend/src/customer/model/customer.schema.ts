7487adc3560859ec2ff875af9363ae558c54dccd
NMTThinh <129759845+NMTThinh@users.noreply.github.com>
2025-04-22 16:38:40 +0700

    Update customer.schema.ts

--- ThucTapTotNghiep/QuanLyGiatSay/Back_end/backend/src/customer/model/customer.schema.ts
+++ ThucTapTotNghiep/QuanLyGiatSay/Back_end/backend/src/customer/model/customer.schema.ts
@@ -1,40 +1,40 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document, SchemaTypes, Types } from 'mongoose';
-import { Role } from 'src/auth/decorator/role.enum';
+  import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+  import { Document, SchemaTypes, Types } from 'mongoose';
+  import { Role } from 'src/auth/decorator/role.enum';
 
-export type CustomerDocument = Customer & Document;
+  export type CustomerDocument = Customer & Document;
 
-@Schema({ timestamps: true }) // tự động thêm createdAt & updatedAt
-export class Customer {
-  @Prop({ type: SchemaTypes.ObjectId })
-  _id: Types.ObjectId;
+  @Schema({ timestamps: true }) // tự động thêm createdAt & updatedAt
+  export class Customer {
+    @Prop({ type: SchemaTypes.ObjectId })
+    _id: Types.ObjectId;
 
-  @Prop({ required: true })
-  name: string;
+    @Prop({ required: true })
+    name: string;
 
-  @Prop({ required: true })
-  phoneNumber: string;
+    @Prop({ required: true })
+    phoneNumber: string;
 
-  @Prop({ unique: true })
-  email?: string;
+    @Prop({ unique: true })
+    email?: string;
 
-  @Prop()
-  address?: string;
+    @Prop()
+    address?: string;
 
-  @Prop()
-  birth_date?: Date;
+    @Prop()
+    birth_day?: Date;
 
-  @Prop({ required: true })
-  password: string;
-  
-  @Prop()
-  customer_type?: string;
+    @Prop({ required: true })
+    password: string;
+    
+    @Prop()
+    customer_type?: string;
 
-  @Prop({ type: String, enum: Role, default: Role.CUSTOMER })
-  role: Role;
+    @Prop({ type: String, enum: Role, default: Role.CUSTOMER })
+    role: Role;
 
-  @Prop({ default: true })
-  status: boolean;
-}
+    @Prop({ default: true })
+    status: boolean;
+  }
 
-export const CustomerSchema = SchemaFactory.createForClass(Customer);
+  export const CustomerSchema = SchemaFactory.createForClass(Customer);
