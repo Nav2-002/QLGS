bf87651da3236827e476eb63c97afe1883bd4f42
NMTThinh <129759845+NMTThinh@users.noreply.github.com>
2025-04-24 15:00:54 +0700

    Update service.repository.ts

--- ThucTapTotNghiep/QuanLyGiatSay/Back_end/backend/src/service/service.repository.ts
+++ ThucTapTotNghiep/QuanLyGiatSay/Back_end/backend/src/service/service.repository.ts
@@ -1,4 +1,3 @@
-// service.repository.ts
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
@@ -10,23 +9,53 @@ import { Service } from './model/service.schema';
 export class ServiceRepository {
   constructor(@InjectModel(Service.name) private readonly model: Model<Service>) {}
 
+  /**
+   * Tạo mới dịch vụ.
+   * @param service Dữ liệu tạo mới dịch vụ.
+   * @returns Dịch vụ đã tạo.
+   */
   async create(service: CreateServiceDto) {
     const newService = new this.model({ _id: new Types.ObjectId(), ...service });
     return await newService.save();
   }
 
+  /**
+   * Tìm dịch vụ theo ID.
+   * @param id ID của dịch vụ.
+   * @returns Dịch vụ hoặc null nếu không tìm thấy.
+   */
   async findOne(id: string) {
     return this.model.findOne({ _id: id }).lean<Service>(true);
   }
 
+  /**
+   * Cập nhật thông tin dịch vụ theo ID.
+   * @param id ID của dịch vụ cần cập nhật.
+   * @param _ Thông tin dịch vụ cũ (không được sử dụng trong hàm).
+   * @param serviceNew Dữ liệu mới để cập nhật dịch vụ.
+   * @returns Dịch vụ đã được cập nhật.
+   */
   async updateOne(id: string, _: Service, serviceNew: UpdateServiceDto) {
     return this.model.findOneAndUpdate({ _id: id }, serviceNew, { new: true });
   }
 
+  /**
+   * Xóa dịch vụ theo ID.
+   * @param id ID của dịch vụ cần xóa.
+   * @returns Kết quả của thao tác xóa.
+   */
   async deleteOne(id: string) {
     return this.model.findOneAndDelete({ _id: id });
   }
 
+  /**
+   * Tìm tất cả dịch vụ có phân trang, sắp xếp và tìm kiếm.
+   * @param page Số trang.
+   * @param limit Số lượng dịch vụ trên mỗi trang.
+   * @param sort Thứ tự sắp xếp ('asc' hoặc 'desc').
+   * @param keyword Từ khóa tìm kiếm (trong trường 'name').
+   * @returns Mảng các dịch vụ thỏa mãn điều kiện.
+   */
   async findAll(
     page: number,
     limit: number,
@@ -41,7 +70,12 @@ export class ServiceRepository {
       .lean<Service[]>(true);
   }
 
+  /**
+   * Lấy danh sách tất cả tên dịch vụ.
+   * @returns Mảng các dịch vụ với chỉ thông tin tên.
+   */
   async findAllGetName() {
-    return this.model.find().lean<Service[]>(true);
+    return await this.model.find().lean<Service[]>(true);
   }
 }
+
